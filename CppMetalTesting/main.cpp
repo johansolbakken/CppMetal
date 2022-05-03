809be575ea77093247c53ec7ad0b4e9fdae2bd70
Johan Solbakken <johan.hvaler@gmail.com>
2022-05-03 12:30:59 +0200

    Initial Commit

---
+++ CppMetalTesting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppMetalTesting
+//
+//  Created by Johan Solbakken on 03/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
